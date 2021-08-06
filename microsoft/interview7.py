b6d0032c2f6ee7c1c0dc3e0227559b70080c6f45
wangzelin007 <1064534588@qq.com>
2021-08-06 23:05:26 +0800

    commit

--- microsoft/interview7.py
+++ microsoft/interview7.py
@@ -1,22 +1,46 @@
+import random
+import string
+import unittest
+
+
 class Solution:
     def find_first_repeat(self, word: str) -> str:
         for id, w in enumerate(word):
             if w in word[id + 1: len(word)]:
                 return w
 
+    def find_first_repeat_by_dict(self, word: str) -> str:
+        dic = {}
+        opt = float('inf')
+        for id, w in enumerate(word):
+            if w in dic and dic[w] < opt:
+                opt = dic[w]
+            else:
+                dic[w] = id
+        return word[opt] if opt < len(word) else None
 
-def test():
-    s = Solution()
-    # repeat at last
-    assert s.find_first_repeat('catyzyxy') == 'y'
-    # repeat at first
-    assert s.find_first_repeat('cccyaty') == 'c'
-    # repeat at middle
-    assert s.find_first_repeat('message') == 'e'
-    # multi repeat return the first one
-    assert s.find_first_repeat('message') != 's'
-    # no repeat
-    assert s.find_first_repeat('bed') is None
+
+class TestSolution(unittest.TestCase):
+    def test_base_case(self):
+        s = Solution()
+        # repeat at last
+        assert s.find_first_repeat('catyzyxy') == 'y'
+        # repeat at first
+        assert s.find_first_repeat('cccyaty') == 'c'
+        # repeat at middle
+        assert s.find_first_repeat('message') == 'e'
+        # multi repeat return the first one
+        assert s.find_first_repeat('message') != 's'
+        # no repeat
+        assert s.find_first_repeat('bed') is None
+
+    def test_by_compare(self):
+        s = Solution()
+        for i in range(100000):
+            word = ''.join([random.choice(string.ascii_lowercase)
+                            for _ in range(random.randint(1, 10))])
+            assert s.find_first_repeat(word) == s.find_first_repeat_by_dict(word)
+        print("Pass 100000 test cases")
 
 if __name__ == '__main__':
-    test()
+    unittest.main()
