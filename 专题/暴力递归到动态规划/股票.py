482f4dc74ae51b110243a49c711892e002382241
wangzelin007 <1064534588@qq.com>
2021-08-11 17:49:27 +0800

    commit

--- 专题/暴力递归到动态规划/股票.py
+++ 专题/暴力递归到动态规划/股票.py
@@ -1,17 +1,16 @@
-import sys
-#str = raw_input()
-#print str
-def get_stock_profite(li):
-    profite = 0
+def get_stock_profit(li):
+    profit = 0
     for i in range(1, len(li)):
         sell_price = li[i]
         buy_price = min(li[:i])
-        profite = max(profite, sell_price - buy_price)
-    return profite
+        profit = max(profit, sell_price - buy_price)
+    return profit
+
 
 def test():
-    li = [4,1,3,4,7,8,9,20,4]
-    assert get_stock_profite(li) == 19
+    li = [4, 1, 3, 4, 7, 8, 9, 20, 4]
+    assert get_stock_profit(li) == 19
+
 
 if __name__ == '__main__':
     test()
---
+++ 专题/暴力递归到动态规划/股票问题合集.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Wang Zelin (王泽霖)
+    :license: MIT, see LICENSE for more details.
+"""
